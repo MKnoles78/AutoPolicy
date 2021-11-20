6c8e1d0d886dc03742f6c051015b68318626897a
Mike Knoles <mknoles78@gmail.com>
2021-11-20 13:43:04 -0500

    added AutoPolicyTest

--- src/AutoPolicyTest.java
+++ src/AutoPolicyTest.java
@@ -1,9 +1,21 @@
-
+// Demonstrating Strings in switch
 public class AutoPolicyTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// create two AutoPolicy objects
+		AutoPolicy policy1 = new AutoPolicy(11111111, "Toyoto Camry", "NJ");
+		AutoPolicy policy2 = new AutoPolicy(22222222, "Ford Fusion", "ME");
 
+		// display whether each policy is in a no-fault state
+		policyInNoFaultState(policy1);
+		policyInNoFaultState(policy2);
 	}
 
+	// method that display whether an AutoPolicy
+	// is in a state with no-fault auto insurance
+	public static void policyInNoFaultState(AutoPolicy policy) {
+		System.out.println("The auot policy:");
+		System.out.printf("Account #: %d; Car: %s;%nState %s %s no-fault state%n%n", policy.getAccountNumber(),
+				policy.getMakeAndModel(), policy.getState(), (policy.isNoFaultState() ? "is" : "is not"));
+	}
 }
