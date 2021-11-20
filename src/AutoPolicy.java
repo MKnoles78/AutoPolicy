10dd77f27a214a0615218199bc56008ab6bc0e3c
Mike Knoles <mknoles78@gmail.com>
2021-11-20 13:09:20 -0500

    Updated attributes in class

--- src/AutoPolicy.java
+++ src/AutoPolicy.java
@@ -1,4 +1,7 @@
 //Class that represents an auto insurance policy
 public class AutoPolicy {
+	private int accountNumber; // policy account number
+	private String makeAndModel; // car that the policy applies to
+	private String state; // two-letter state abbreviation
 
 }
