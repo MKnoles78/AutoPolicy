831e09737ec036f85bf2daf38c45ff72dcc2b859
Mike Knoles <mknoles78@gmail.com>
2021-11-20 12:58:05 -0500

    correcting class

--- src/AutoPolicy.java
+++ src/AutoPolicy.java
@@ -1,7 +1,4 @@
 //Class that represents an auto insurance policy
 public class AutoPolicy {
-	private int accountNumber; // policy account number
-	private String makeAndModel; // car that the policy applies to
-	private String state; // two-letter state abbreviation
 
 }
